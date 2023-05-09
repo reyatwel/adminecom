2a8ca5f2d58c89ad0f8cfb299c558dad390d77be
reyatwel <sysdev@gmx.com>
2023-05-09 18:19:27 +0800

    Added API for Home Slider

---
+++ app/Http/Controllers/Admin/SliderController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\HomeSlider;
+
+class SliderController extends Controller
+{
+    public function AllSlider()
+    {
+        $result = HomeSlider::all();
+
+        return $result;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Admin\ContactController;
 use App\Http\Controllers\Admin\SiteInfoController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\ProductListController;
+use App\Http\Controllers\Admin\SliderController;
 
 /*
 |--------------------------------------------------------------------------
@@ -63,4 +64,10 @@ Route::get(
 Route::get(
     '/productlistbysubcategory/{category}/{subcategory}',
     [ProductListController::class, 'ProductListBySubcategory']
+);
+
+// Slider route
+Route::get(
+    '/allslider',
+    [SliderController::class, 'AllSlider']
 );
