8bae0cb9e4a24b8082a3be334d55e0a73c2c2156
reyatwel <sysdev@gmx.com>
2023-05-11 15:30:07 +0800

    Added User Profile API

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,7 @@ use App\Http\Controllers\Admin\NotificationController;
 use App\Http\Controllers\User\AuthController;
 use App\Http\Controllers\User\ForgetController;
 use App\Http\Controllers\User\ResetController;
+use App\Http\Controllers\User\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -110,4 +111,7 @@ Route::post('/forgetpassword', [ForgetController::class, 'ForgetPassword']);
 
 // Reset Password Routes
 Route::post('/resetpassword', [ResetController::class, 'ResetPassword']);
+
+// Current User Route
+Route::get('/user', [UserController::class, 'User'])->middleware('auth:api');
 //
