f0565c25d3856b8a422990ad35e5653c2750d6a2
reyatwel <sysdev@gmx.com>
2023-05-13 15:26:37 +0800

    Updated ProductCart Controller

--- app/Http/Controllers/Admin/ProductCartController.php
+++ app/Http/Controllers/Admin/ProductCartController.php
@@ -101,54 +101,51 @@ class ProductCartController extends Controller
     public function CartOrder(Request $request)
     {
         $city = $request->input('city');
-        $paymentMethod = $request->input('paymentMethod');
+        $paymentMethod = $request->input('payment_method');
         $yourName = $request->input('name');
         $email = $request->input('email');
-        $deliveryAddress = $request->input('delivery_address');
-        $invoiceNo = $request->input('invoice_no');
-        $deliveryCharge = $request->input('delivery_charge');
+        $DeliveryAddress = $request->input('delivery_address');
+        $invoice_no = $request->input('invoice_no');
+        $DeliveryCharge = $request->input('delivery_charge');
 
-        date_default_timezone_set('Asia/Manila');
+        date_default_timezone_set("Asia/Dhaka");
+        $request_time = date("h:i:sa");
+        $request_date = date("d-m-Y");
 
-        $request_time = date('h:i:sa');
-        $request_date = date('d-m-Y');
+        $CartList = ProductCart::where('email', $email)->get();
 
-        $cartList = ProductCart::where('email', $email)->get();
-
-        foreach ($cartList as $order) {
+        foreach ($CartList as $CartListItem) {
             $cartInsertDeleteResult = "";
 
-            $result = CartOrder::insert([
-                'invoice_no' => 'PVBI' . $invoiceNo,
-                'product_name' => $order['product_name'],
-                'product_code' => $order['product_code'],
-                'size' => $order['size'],
-                'color' => $order['color'],
-                'quantity' => $order['quantity'],
-                'unit_price' => $order['unit_price'],
-                'total_price' => $order['total_price'],
-                'email' => $order['email'],
+            $resultInsert = CartOrder::insert([
+                'invoice_no' => "Easy" . $invoice_no,
+                'product_name' => $CartListItem['product_name'],
+                'product_code' => $CartListItem['product_code'],
+                'size' => $CartListItem['size'],
+                'color' => $CartListItem['color'],
+                'quantity' => $CartListItem['quantity'],
+                'unit_price' => $CartListItem['unit_price'],
+                'total_price' => $CartListItem['total_price'],
+                'email' => $CartListItem['email'],
                 'name' => $yourName,
                 'payment_method' => $paymentMethod,
-                'delivery_address' => $deliveryAddress,
+                'delivery_address' => $DeliveryAddress,
                 'city' => $city,
-                'delivery_charge' => $deliveryCharge,
+                'delivery_charge' => $DeliveryCharge,
                 'order_date' => $request_date,
                 'order_time' => $request_time,
-                'order_status' => 'Pending'
+                'order_status' => "Pending",
             ]);
 
-            if ($result === 1) {
-                $delete = ProductCart::where('id', $order['id'])->delete();
-
-                if ($delete === 1) {
+            if ($resultInsert == 1) {
+                $resultDelete = ProductCart::where('id', $CartListItem['id'])->delete();
+                if ($resultDelete == 1) {
                     $cartInsertDeleteResult = 1;
                 } else {
                     $cartInsertDeleteResult = 0;
                 }
             }
         }
-
         return $cartInsertDeleteResult;
     } //
 }
